e0672cc4dde7e453c199d032418625f852168ad4
Vinicius Hansel <hanelvinicius@gmail.com>
2026-09-06 22:26:56 -0300

    debug: dbug logs

--- src/commands/CallBroosCommand.ts
+++ src/commands/CallBroosCommand.ts
@@ -12,7 +12,7 @@ export class CallBroosCommand {
     private async callBroos(message: Message, args: string[]) {
         try {
             const url = process.env.NOCTIS_API_URL || "http://localhost:3000/api/whatsapp/call";
-            await fetch(url, {
+            const res = await fetch(url, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -22,6 +22,7 @@ export class CallBroosCommand {
                     message: `CHAMADO PARA O DISCORD MENSAGEM: ${args.join(", ")}`,
                 })
             });
+            console.log(`Broos API response: ${res.status} ${res}`);
             await message.reply(`Calling Broos with args: ${args.join(", ")}`);
         } catch (error) {
             await message.reply("Failed to call Broos. Please try again later.");
